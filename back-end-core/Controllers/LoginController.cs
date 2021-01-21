8496dc34120c8be95475c65d165a8fbee5f692d0
AndreMesquita <twigandre@gmail.com>
2021-01-21 00:14:29 -0400

    inclsao back-end. .net core 3.0

---
+++ back-end-core/Controllers/LoginController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace back_end_core.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+
+        public LoginController()
+        {
+
+        }
+
+        [HttpPost]
+        public int Post([FromBody] object objeto)
+        {
+            return 400;
+        }
+
+
+    }
+}
